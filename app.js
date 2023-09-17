9a147eec761319866d0b31588a2c0af5d1a336ea
Sangir <sangir.iskandarov@gmail.com>
2023-09-17 15:16:07 +0500

    JS

---
+++ app.js
@@ -0,0 +1,23 @@
+var login = prompt('Введите ваше имя')
+console.log('Ваше имя ' + login);
+
+var age = prompt('Введите ваш возраст')
+console.log('Ваш возраст ' + age +  ' лет/года');
+
+var plus = prompt('Пример 1: 5 + 7 = ')
+console.log('Пример 1: 5 + 7 = 12 . Ваш ответ ' + plus);
+
+var minus = prompt('Пример 2: 10 - 3 = ')
+console.log('Пример 2: 10 - 3 = 7 . Ваш ответ ' + minus);
+
+var division = prompt('Пример 3: 99 / 3 = ')
+console.log('Пример 3: 99 / 3 = 33 . Ваш ответ ' + division);
+
+var multiplication = prompt('Пример 4: 12 * 5 = ')
+console.log('Пример 4: 12 * 5 = 60 . Ваш ответ ' + multiplication);
+
+var remains = prompt('Пример 5: 15 % 7  = ')
+console.log('Пример 5: 15 % 7  = 1 . Ваш ответ ' + remains);
+
+alert('Откройте консуль для просмотра ответов')
+
